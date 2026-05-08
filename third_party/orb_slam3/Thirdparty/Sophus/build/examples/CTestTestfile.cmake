778eb6cf4015f5a22a9d2af169a49dff777e4571
Sanghyun Ryoo <sanghyunryoo@postech.ac.kr>
2026-05-08 19:38:16 +0900

    feat(major): add ignored file

---
+++ third_party/orb_slam3/Thirdparty/Sophus/build/examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/ros2_ws/src/ORB_SLAM3/Thirdparty/Sophus/examples
+# Build directory: /root/ros2_ws/src/ORB_SLAM3/Thirdparty/Sophus/build/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ third_party/orb_slam3/Thirdparty/Sophus/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/ros2_ws/src/ORB_SLAM3/Thirdparty/Sophus/test
+# Build directory: /root/ros2_ws/src/ORB_SLAM3/Thirdparty/Sophus/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("ceres")
